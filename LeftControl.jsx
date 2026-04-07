fd04211bd7021997b466915eb0bab8bc896eae08
karencassani <a01640798@tec.mx>
2026-04-06 20:19:36 -0600

    LEFT BOTON

--- LeftControl.jsx
+++ LeftControl.jsx
@@ -1,12 +1,22 @@
+// 
+
 import React from 'react'
 
 function LeftControl() {
     return (
-        <div>
-            <div className= "w-[50px] h-[180px] border-4 border-solid" ></div>
-            </div>   
-    )
+        <div className="w-[80px] h-[240px] border-4 border-solid  relative">
 
+            <div className="absolute top-5 left-1/2 -translate-x-1/2 w-10 h-10 border-4 rounded-full"></div>
+
+            { <div className="absolute top-20 left-1/2 -translate-x-1/2 w-14 h-14 relative">
+                <div className="absolute top-0 left-1/2 -translate-x-1/2 w-5 h-5 border-2 rounded-full"></div>
+                <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-5 h-5 border-2 rounded-full"></div>
+                <div className="absolute left-0 top-1/2 -translate-y-1/2 w-5 h-5 border-2 rounded-full"></div>
+                <div className="absolute right-0 top-1/2 -translate-y-1/2 w-5 h-5 border-2 rounded-full"></div>
+            </div> }
+
+        </div>
+    )
 }
-        
+
 export default LeftControl
