22b3820de61d8968366f3f69d25733090a51ea8a
karencassani <a01640798@tec.mx>
2026-04-07 20:49:28 -0600

    updated app.jsx

--- App.jsx
+++ App.jsx
@@ -1,25 +1,87 @@
-import {useState} from 'react'
-import './App.css'
-import Screen from './components/Screen'
-import useFetch from './hooks/useFetch';
-import RightControl from './components/RightControl';
+import './App.css';
+import Screen from  './components/Screen';
+import GameScreen from './components/GameScreen';
 import LeftControl from './components/LeftControl';
+import RightControl from './components/RightControl';
+import useFetch from './hooks/useFetch';
+import { useEffect, useState } from 'react';
+
 
 function App() {
-  const url="https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
-  const {data, loading, error}= useFetch(url);
+  const url = 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0';
+  const { data, loading, error } = useFetch(url);
 
-  console.log(data);
+  // sanitizer data 
+  const [pokemones, setPokemones] = useState([]);
+  const getListPokemones = () => {
+    const list = data?.results?.filter((p) => p.url);
+    const plist = list?.map((l) => fetch(l.url).then((res) => res.json()));
+    Promise.all(plist).then((values) => {
+      // console.log('promesa values', values);
+      setPokemones(values);
+    });
+  };
 
+  useEffect(() => {
+    getListPokemones();
+  }, [data]);
+
+  // Handle Direction
+  const [position, setPosition] = useState(1);
+
+  const [myPokeSelection, setMyPokeSelection] = useState([]);
+  const [pcPokeSelection, setPcPokeSelection] = useState([]);
+
+  const handleDirection = (direction) => {
+    
+    if (direction === 'right' && position + 1 < 101) {
+      setPosition((prev) => prev + 1);
+    } else if(direction === 'left' && position - 1 > 0){
+      setPosition((prev) => prev - 1);
+    } else if(direction === 'down' && position + 4 < 101){
+      setPosition((prev) => prev + 4);
+    }else if(direction === 'up' && position - 4 > 0){
+      setPosition((prev) => prev - 4);
+    }else{
+      setPosition(1);
+    }
+  };
+
+  function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  }
+
+  const computerSelection = () => {
+    const rnd = getRandomInt(1, 100);
+    const pc = pokemones.filter((p) => p.id === rnd);
+    // console.log(pc);
+    setPcPokeSelection(pc);
+  }
+
+  const handleSelection = () => {
+    const selectPokemon = pokemones.filter((p) => p.id === position)
+    // console.log(selectPokemon);
+    setMyPokeSelection(selectPokemon);
+    computerSelection();
+  }
+
+  console.log("my",myPokeSelection.length, myPokeSelection)
+  console.log("pc",pcPokeSelection.length, pcPokeSelection)
   return (
-    <div className="flex">
-      <h1 className='text-3xl font-bold underline '> hello world</h1> 
-      <LeftControl/>
-      <Screen pokemones={data?.results}/>
-      <RightControl/>
-  </div>
+    <div className="flex items-center justify-center min-h-screen">
+      <LeftControl handleDirection={handleDirection} />
+      {myPokeSelection.length && pcPokeSelection.length ? (
+        <GameScreen myPokeSelection={myPokeSelection} pcPokeSelection={pcPokeSelection}/>
+      ) : (
+        <Screen pokemones={pokemones} position={position} />
+      )}
+
+
+      <RightControl handleSelection={handleSelection} />
+    </div>
   );
-};
+}
 
-export default App
-
+export default App;
