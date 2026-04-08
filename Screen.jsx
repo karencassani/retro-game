f8744f470af25b8eff57f94d2886eca861943889
karencassani <a01640798@tec.mx>
2026-04-07 20:50:34 -0600

    Screen update

--- Screen.jsx
+++ Screen.jsx
@@ -1,14 +1,25 @@
-const Screen=({pokemones})=>{
-    return(
-      <>
-        <div className="w-[450] h-[200px] border-4 border-solid">
-            {pokemones?.map((pokemon) => (
-                <p>{pokemon.name}</p>
-            ))}
-
+const Screen = ({ pokemones, position }) => {
+  
+  return (
+    <>
+      <div className="w-[450px] h-[240px] overflow-y-auto border-4 border-solid">
+        <div className="flex flex-wrap items-center justify-center">
+          {pokemones?.map((pokemon, index) => (
+            <div key={index}
+              style={{color: position === pokemon.id ? "red" : "white"}}
+              className="flex flex-col border-2" >
+              <img
+                src={pokemon?.sprites?.front_default}
+                alt={pokemon.name}
+                className="w-25 h-25"
+              />
+              <p>{pokemon.name}</p>
+            </div>
+          ))}
         </div>
-     </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Screen;
