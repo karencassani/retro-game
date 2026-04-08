1d015f8f115799c0d8fa3ede3b92b83b9c8d6794
karencassani <a01640798@tec.mx>
2026-04-07 20:49:57 -0600

    Left control update

--- LeftControl.jsx
+++ LeftControl.jsx
@@ -1,22 +1,45 @@
-// 
-
 import React from 'react'
 
-function LeftControl() {
-    return (
-        <div className="w-[80px] h-[240px] border-4 border-solid  relative">
+function LeftControl({handleDirection}) {
+  return (
+    <div className=" relative w-[100px] h-[240px] border-4 rounded-l-3xl border-solid bg-blue-500 ">
 
-            <div className="absolute top-5 left-1/2 -translate-x-1/2 w-10 h-10 border-4 rounded-full"></div>
+      <div className="absolute -top-4 left-2 w-20 h-4 bg-black rounded-t-xl"></div>
 
-            { <div className="absolute top-20 left-1/2 -translate-x-1/2 w-14 h-14 relative">
-                <div className="absolute top-0 left-1/2 -translate-x-1/2 w-5 h-5 border-2 rounded-full"></div>
-                <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-5 h-5 border-2 rounded-full"></div>
-                <div className="absolute left-0 top-1/2 -translate-y-1/2 w-5 h-5 border-2 rounded-full"></div>
-                <div className="absolute right-0 top-1/2 -translate-y-1/2 w-5 h-5 border-2 rounded-full"></div>
-            </div> }
+        <button className="absolute top-0 right-1 w-5 h-5 text-gray text-2xl bg-transparent">-</button>
+
+        <div className="absolute top-12 absolute left-6 relative w-20 h-20">
+
+          <div className="absolute left-0 top-1/9 -translate-y-1/2 w-10 h-10 bg-gray-800 rounded-full flex items-center justify-center text-white text-xs"></div>
 
         </div>
-    )
+
+          <div className="absolute top-5 absolute left-1.5 relative w-20 h-20">
+
+            <button 
+            onClick={() => handleDirection('top')} 
+            className="absolute top-0 left-1/2 -translate-x-1/2 w-6 h-6 bg-gray-800 rounded-full flex items-center justify-center text-white text-xs">{'^'}</button>
+
+
+            <button 
+            onClick={() => handleDirection('down')} 
+            className="absolute bottom-0 left-1/2 -translate-x-1/2 w-6 h-6 bg-gray-800 rounded-full flex items-center justify-center text-white text-xs">{'v'}</button>
+
+
+            <button 
+            onClick={() => handleDirection('left')} 
+            className="absolute left-0 top-1/2 -translate-y-1/2 w-6 h-6 bg-gray-800 rounded-full flex items-center justify-center text-white text-xs">{'<'}</button>
+
+
+            <button 
+
+               onClick={() => handleDirection('right')} 
+              className="absolute right-0 top-1/2 -translate-y-1/2 w-6 h-6 bg-gray-800 rounded-full flex items-center justify-center text-white text-xs">{'>'}</button>
+        </div>
+
+          <div className="absolute right-2 top-49 -translate-y-1/2 w-4 h-4 bg-gray-800 flex items-center justify-center text-white text-xs"></div>
+    </div>
+  )
 }
 
 export default LeftControl
